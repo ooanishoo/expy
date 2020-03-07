575e20f8beb89129622d1a757a498319c25242f7
ooanishoo <anish.maharjan12341@gmail.com>
2020-03-07 18:01:51 +1100

    Add companyTypes to select type of company for `add company` form

---
+++ src/data/companyTypes.js
@@ -0,0 +1,17 @@
+export const companyTypes = [
+  {
+    id: 1,
+    type: 'host-company',
+    name: 'Host Company'
+  },
+  {
+    id: 2,
+    type: 'edu-provider',
+    name: 'Education Provider'
+  },
+  {
+    id: 3,
+    type: 'agency',
+    name: 'Agency'
+  },
+]
