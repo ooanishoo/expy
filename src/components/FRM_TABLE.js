d924474766971fcdd80a3dd14083b289e7039c29
ooanishoo <anish.maharjan12341@gmail.com>
2020-03-08 20:53:18 +1100

    Add editable actions for add, edit and delete

--- src/components/FRM_TABLE.js
+++ src/components/FRM_TABLE.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import MaterialTable from "material-table";
 
-
 export default class FRM_TABLE extends Component {
   constructor(props) {
     super(props);
@@ -16,16 +15,15 @@ export default class FRM_TABLE extends Component {
 
   handleRowClick = (evt, selectedRow) => {
     this.setState({ selected: selectedRow }, () => {
-      console.log('callback', this.state.selected);
-      if(this.props.onRowClick){
+      console.log("callback", this.state.selected);
+      if (this.props.onRowClick) {
         this.props.onRowClick(this.state.selected);
       }
-
     });
-  }
+  };
 
   render() {
-    const {title, columns, data} = this.state;
+    const { title, columns, data } = this.state;
     console.log(this.props);
     return (
       <div>
@@ -33,30 +31,52 @@ export default class FRM_TABLE extends Component {
           title={title}
           columns={columns}
           data={data}
-          actions={[
-            {
-              icon: "add",
-              tooltip: "Add " + title,
-              isFreeAction: true,
-              onClick: event => alert("You want to add a new " + title)
-            },
-            {
-              icon: "remove_red_eye",
-              tooltip: "View Details",
-              onClick: this.handleRowClick
-            },
-            {
-              icon: "delete",
-              tooltip: "Delete Internship",
-              onClick: (event, rowData) =>
-                alert("You want to delete " + rowData.name)
-            }
-          ]}
           options={{
             selection: false,
             columnsButton: false,
             rowStyle: rowData => ({
-                backgroundColor: (this.state.selected && this.state.selected.tableData.id === rowData.tableData.id) ? '#EEE' : '#FFF'
+              backgroundColor:
+                this.state.selected &&
+                this.state.selected.tableData.id === rowData.tableData.id
+                  ? "#EEE"
+                  : "#FFF"
+            })
+          }}
+          editable={{
+            onRowAdd: newData =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    const data = this.state.data;
+                    data.push(newData);
+                    this.setState({ data }, () => resolve());
+                  }
+                  resolve();
+                }, 1000);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    const data = this.state.data;
+                    const index = data.indexOf(oldData);
+                    data[index] = newData;
+                    this.setState({ data }, () => resolve());
+                  }
+                  resolve();
+                }, 1000);
+              }),
+            onRowDelete: oldData =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  {
+                    let data = this.state.data;
+                    const index = data.indexOf(oldData);
+                    data.splice(index, 1);
+                    this.setState({ data }, () => resolve());
+                  }
+                  resolve();
+                }, 1000);
               })
           }}
           onRowClick={this.handleRowClick}
