c88083cc2a72848b1d4f57f14511442b9a4cecf4
ooanishoo <anish.maharjan12341@gmail.com>
2020-03-08 20:54:18 +1100

    Add dummy companies json data

---
+++ src/data/companies.js
@@ -0,0 +1,79 @@
+export const companies = [
+  {
+    name: "Google",
+    email: "google@gmail.com",
+    phone: "0424664610",
+    status: "Hiring",
+    companyType: {
+      id: 1,
+      type: "host-company",
+      name: "Host Company"
+    }
+  },
+  {
+    name: "CQUniversity",
+    email: "cqu@gmail.com",
+    phone: "0424664610",
+    status: "Active",
+    companyType: {
+      id: 2,
+      type: 'edu-provider',
+      name: 'Education Provider'
+    }
+  },
+  {
+    name: "ATMC",
+    email: "atmc@gmail.com",
+    phone: "0424664610",
+    status: "Active",
+    companyType: {
+      id: 2,
+      type: 'edu-provider',
+      name: 'Education Provider'
+    }
+  },
+  {
+    name: "Pwc",
+    email: "pwc@gmail.com",
+    phone: "0424664610",
+    status: "Hiring",
+    companyType: {
+      id: 1,
+      type: "host-company",
+      name: "Host Company"
+    }
+  },
+  {
+    name: "Gada Technologoies",
+    email: "gada@gmail.com",
+    phone: "0424664610",
+    status: "Hiring",
+    companyType: {
+      id: 1,
+      type: "host-company",
+      name: "Host Company"
+    }
+  },
+  {
+    name: "iDibs",
+    email: "idibs@gmail.com",
+    phone: "0424664610",
+    status: "Hiring",
+    companyType: {
+      id: 1,
+      type: "host-company",
+      name: "Host Company"
+    }
+  },
+  {
+    name: "Pencil Rocker",
+    email: "pencilrocket@gmail.com",
+    phone: "0424664610",
+    status: "Hiring",
+    companyType: {
+      id: 1,
+      type: "host-company",
+      name: "Host Company"
+    }
+  },
+];
